20c2350bef0f516a52f8896f8e1829b2c51b7e99
angelodibella <angelo.dibella2020@gmail.com>
2024-01-22 13:16:23 +0000

    Add sample circuit

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import stim
 
-print(stim.__version__)
+import circuit
+
+def save_diagram_svg(circuit: stim.Circuit, filepath: str) -> None:
+    diagram = circuit.diagram('timeline-svg')
+    with open(filepath, 'w') as file:
+        file.write(str(diagram))
+
+bell = circuit.simple_circuit()
+save_diagram_svg(bell, 'temp_results/bell_diagram.svg')
+
+sampler = bell.compile_sampler()
+print(sampler.sample(shots=10))
