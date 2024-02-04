ceb9e43acb011728b858c325822e7bb53c2d3427
angelodibella <angelo.dibella2020@gmail.com>
2024-02-04 22:28:24 +0000

    Add phenomenological noise

--- main.py
+++ main.py
@@ -1,34 +1,8 @@
 import stim
 
-import circuit
+import model
 
-
-def save_diagram(circuit: stim.Circuit, filepath: str, **kwargs) -> None:
-    diagram = circuit.diagram(**kwargs)
-    with open(filepath, "w") as file:
-        file.write(str(diagram))
-
-
-def display_samples(circuit: stim.Circuit, shots: int = 1, rounds=1) -> None:
-    sampler = circuit.compile_sampler().sample(shots)
-    for i, shot in enumerate(sampler, start=1):
-        round_list = []
-        for j, outcome in enumerate(shot):
-            round_list.append("x" if outcome else "_")
-            if (j + 1) % (len(shot) / rounds) == 0:
-                round_list[j] += "\n"
-        print(f"Shot {i}:\n" + "".join(round_list))
-    print("\n")
-
-
-bell = circuit.simple_circuit()
-save_diagram(bell, "temp_results/bell_diagram.svg", type="timeline-svg")
-bell.to_file("temp_results/bell.stim")
-
-sampler = bell.compile_sampler()
-print(sampler.sample(shots=10))
-
-rep_3 = circuit.repetition_code(4, 5, noise_data=[0.1, 0.01])
-print(rep_3, "\n")
-
-display_samples(rep_3, shots=3, rounds=5)
+rep_3_model = model.StabilizerModel()
+rep_3_model.repetition_code(3)
+rep_3_model.print()
+rep_3_model.display_samples()
