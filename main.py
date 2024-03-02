4407b1147537b5366139dab00637c4bf0f1230ee
angelodibella <angelo.dibella2020@gmail.com>
2024-03-02 00:55:50 +0000

    Complete repetition code

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-import stim
-
 import model
 
-rep_3_model = model.StabilizerModel()
-rep_3_model.repetition_code(3)
-rep_3_model.print()
-rep_3_model.display_samples()
+rep_model = model.StabilizerModel(rounds=5)
+
+rep_model.set_noise_qubits(0.02)
+rep_model.set_noise_circuit(0.01)
+
+rep_model.repetition_code(8)
+rep_model.measure()
+
+rep_model.display_samples()
+rep_model.display_detector_samples()
+
+rep_model.circuit.diagram()
