f3b03ca3fd693738d190fac50458af4a53527bd5
Angelo Di Bella <angelo.dibella2020@gmail.com>
2025-07-24 16:03:30 +0200

    Move from Poetry to uv

--- weave/__init__.py
+++ weave/__init__.py
@@ -1,11 +1,10 @@
-from .codes import NoiseModel, ClassicalCode, CSSCode, HypergraphProductCode
+from .codes import NoiseModel, CSSCode, HypergraphProductCode
 from .util import pcm, graph
 
 from . import surface
 
 __all__ = [
     "NoiseModel",
-    "ClassicalCode",
     "CSSCode",
     "HypergraphProductCode",
     "pcm",
